e9fcf8ae40c7dd385300f81abf85e3322dd6e996
Disen-Corder <disensenu@gmail.com>
2026-02-20 12:07:40 +0530

    restore

--- AIML-Project/src/main/java/com/marineguard/MarineguardApplication.java
+++ AIML-Project/src/main/java/com/marineguard/MarineguardApplication.java
@@ -1,13 +1,17 @@
+<<<<<<<< Updated upstream:AIML-Project/src/main/java/com/marineguard/MarineguardApplication.java
 package com.marineguard;
+========
+package com.coralhealth;
+>>>>>>>> Stashed changes:AIML-Project/src/main/java/com/coralhealth/CoralHealthApplication.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarineguardApplication {
+public class CoralHealthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarineguardApplication.class, args);
+		SpringApplication.run(CoralHealthApplication.class, args);
 	}
 
 }
